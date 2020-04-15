6b68443bd2d2dc43d2cea1e0486f6a1bceb77042
m4ik7i <m4ik7i@gmail.com>
2020-04-16 02:37:52 +0900

    enha: add title

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,18 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import { ThemeProvider } from 'theme-ui';
 import theme from '@rebass/preset';
 import { Button } from 'rebass';
 
-export default () => (
+export default () => [
+  <Head>
+    <title>About</title>
+  </Head>,
   <ThemeProvider theme={theme}>
     <Link href="/" as={process.env.BACKEND_URL + '/'}>
       <Button variant="secondary" css={{ margin: '16px 0 0 16px' }}>
         Home
       </Button>
     </Link>
-  </ThemeProvider>
-);
+  </ThemeProvider>,
+];
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import { ThemeProvider } from 'theme-ui';
 import theme from '@rebass/preset';
 import { Button } from 'rebass';
 
-export default () => (
+export default () => [
+  <Head>
+    <title>Home</title>
+  </Head>,
   <ThemeProvider theme={theme}>
     <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
       <Button variant="primary" css={{ margin: '16px 0 0 16px' }}>
         About
       </Button>
     </Link>
-  </ThemeProvider>
-);
+  </ThemeProvider>,
+];
